4c255ca548d34d59336f53d1b8a90039df9300fc
Labasse <labasse@gmail.com>
2022-09-05 11:41:06 +0000

    Permutation sans tiers (défi)

---
+++ src/PermutationSansTiers.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PermutationSansTiers {
+
+  @Test
+  public void test() {
+    byte a = 99;
+    byte b = 101;
+
+    // Votre essai ici
+
+    assertEquals(101, a);
+    assertEquals(99, b);
+  }
+}
