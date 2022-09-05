bfd123dadeb6f2c575d8b8d872db999bf0aaa3cf
Labasse <labasse@gmail.com>
2022-09-05 12:35:05 +0000

    Désintégration audioactive (solution)

--- src/Audioactive.java
+++ src/Audioactive.java
@@ -1,6 +1,24 @@
 public class Audioactive {
 
   public static long valeurSuivante(long valeur) {
-    throw new UnsupportedOperationException("Essayez votre propre solution ici !!!");
+    long resultat = 0;
+    long prec = valeur % 10;
+    long puissance100 = 1;
+    int compteur = 1;
+
+    do {
+      valeur /= 10;
+
+      long chiffre = valeur % 10;
+
+      if (chiffre != prec || valeur == 0) {
+        resultat += (10 * compteur + prec) * puissance100;
+        prec = chiffre;
+        compteur = 0;
+        puissance100 *= 100;
+      }
+      compteur++;
+    } while (valeur > 0);
+    return resultat;
   }
 }
