6b43f7ca46a6e0c98f9afb7b96ecb193724712d7
Labasse <labasse@gmail.com>
2022-09-07 13:38:52 +0000

    Sans commentaire (solution)

--- src/SansCommentaire.java
+++ src/SansCommentaire.java
@@ -20,127 +20,160 @@ import java.util.Scanner;
 
 public class SansCommentaire {
 
-	/////////////////////////////////////////////////////////////////
-	// Constantes du programme
-	//
-	public static final int TAILLE = 3; // Taille du plateau de jeu (NE PEUT PAS DÉPASSER 9)
-	public static final String PIONS = " xo"; // Caractères utilisés pour les pions	
-	public static final Direction [] DIRECTIONS = { // Directions pour recherche de victoire
-		//            dx, dy 
-		new Direction( 1,  0), // Est
-		new Direction( 1,  1), // Sud-Est
-		new Direction( 0,  1)  // Sud		
-	}; 
-	
-	public static void main(String[] args) {
-		int [][] plateau = new int[TAILLE][TAILLE]; // 0 : vide, 1 : joueur 1, 2 : joueur 2
-		int gagnant = 0; // Initialisation obligatoire, le compilateur ne détecte pas l'initialisation dans for(:)
-		int joueur = 1;
-		int casesLibres = TAILLE*TAILLE;
-		Scanner clavier = new Scanner(System.in); 
-		
-		do
-		{
-			// Dessin du plateau
-			for(int i=0; i<TAILLE; i++) {
-				System.out.printf("  %c ", 'A'+i);
-			}
-			System.out.println();
-			for(int i=0; i<TAILLE; i++) { // Lignes
-				dessinerLigne(TAILLE);
-				for(int j=0; j<TAILLE; j++) { // Colonnes
-					System.out.printf("| %c ", PIONS.charAt(plateau[j][i]));
-				}
-				System.out.printf("| %d\n", i+1);
-			}
-			dessinerLigne(TAILLE);
-			
-			// Récupération du choix du joueur, on recommence tant que ce n'est pas valide
-			int x=-1, y=-1;
-			do {
-				System.out.printf("Joueur %d, quelle case (ex: A1) ? ", joueur);
-				String position = clavier.next();
-				
-				try {
-					x = position.charAt(0)-'A';
-					y = position.charAt(1)-'1';
-				}
-				catch(Exception e) {
-					// Mauvais formatage
-					System.err.println("Saisie incorrecte");
-				}
-			}
-			while(x < 0 || TAILLE <= x || y < 0 || TAILLE <= y || plateau[x][y]!=0);
-			
-			// Placement du pion et recherche du gagnant
-			plateau[x][y] = joueur;
-			casesLibres--;
-			for(Direction d : DIRECTIONS) {
-				gagnant = d.getJoueurGagnant(plateau);
-				if(gagnant > 0) {
-					break;
-				}
-			}
-			
-			// Changement de joueur
-			joueur = 3 - joueur; 
-		}
-		while(gagnant == 0 && casesLibres > 0);
-		
-		// TODO : Afficher le plateau en fin de partie
-		
-		System.out.printf(gagnant==0 
-			? "Match nul !" 
-			: "Bravo joueur %d !", gagnant
-		);
-		clavier.close();
-	}
-	
-	///////////////////////////////////////////
-	// Classe Direction
-	// 
-	public static class Direction {
-		// Champs privés
-		private final int dx;
-		private final int dy;
-		
-		/**
-		 * Initialise une direction utilisée pour la recherche de victoire 
-		 * @param dx Direction horizontale
-		 * @param dy Direction verticale
-		 */
-		public Direction(int dx, int dy) {
-			this.dx = dx;
-			this.dy = dy;
-		}
-		
-		// Méthodes
-		
-		/**
+  public static final int TAILLE = 3; // NE PEUT PAS DÉPASSER 9 car la saisie utilisateur ne supporte pas plus d'un
+                                      // chiffre
+  public static final String PIONS = " xo";
+  public static final Direction[] DIRECTIONS = {
+      //            dx, dy
+      new Direction(1, 0), // Est
+      new Direction(1, 1), // Sud-Est
+      new Direction(0, 1) // Sud
+  };
+  private static final int AUCUN = 0;
+  private static final int JOUEUR1 = 1;
+  private static final int JOUEUR2 = 2;
+
+  public static void main(String[] args) {
+    int[][] plateau = new int[TAILLE][TAILLE];
+    int joueur = JOUEUR1;
+    int gagnant;
+    int casesLibres = TAILLE * TAILLE;
+    Scanner clavier = new Scanner(System.in);
+
+    do {
+      dessinerPlateau(plateau);
+
+      Position coup = Position.lire(plateau, String.format("Joueur %d", joueur), clavier);
+
+      plateau[coup.getX()][coup.getY()] = joueur;
+      casesLibres--;
+      gagnant = Direction.getGagnant(plateau, DIRECTIONS);
+
+      joueur = adversaire(joueur);
+    } while (gagnant == AUCUN && casesLibres > 0);
+
+    dessinerPlateau(plateau);
+
+    System.out.printf(gagnant == AUCUN
+        ? "Match nul !"
+        : "Bravo joueur %d !", gagnant);
+    clavier.close();
+  }
+
+  public static class Direction {
+    private final int dx;
+    private final int dy;
+
+    /**
+     * Initialise une direction utilisée pour la recherche de victoire
+     * @param dx Direction horizontale
+     * @param dy Direction verticale
+     */
+    public Direction(int dx, int dy) {
+      this.dx = dx;
+      this.dy = dy;
+    }
+
+    /**
 		 * Recherche un gagnant dans cette direction
 		 * @param plateau Plateau sur lequel chercher le joueur gagnant
-		 * @return Numéro du joueur gagnant, 0 si aucun
+		 * @return Joueur gagnant, "Aucun" si aucun
 		 */
 		public int getJoueurGagnant(int [][] plateau) {
 			int joueur = plateau[0][0];
 			
 			for(int i=1; i<TAILLE; i++) {
-				// ième case selon la direction
-				if(plateau[i*dx][i*dy] != joueur) {
-					return 0;
+				if(ieme(plateau, i) != joueur) {
+					return AUCUN;
 				}
 			}
 			return joueur;
 		}
-	}
-	
-	//////////////////////////////////////////////////////
-	// Méthodes de dessin
-	private static void dessinerLigne(int taille) {
-		for(int j=0; j<taille; j++) {
-			System.out.print("+---");
+
+    /**
+     * Recherche si un joueur a aligné tous ses pions dans une des directions
+     * fournies.
+     * 
+     * @param plateau    Plateau sur lequel l'alignement doit être cherché
+     * @param directions Tableau des directions sur lesquelles chercher l'alignement
+     * @return Le joueur gagnant, "Aucun" si aucun
+     */
+    public static int getGagnant(int[][] plateau, Direction[] directions) {
+      int gagnant = AUCUN;
+
+      for (Direction d : directions) {
+        gagnant = d.getJoueurGagnant(plateau);
+        if (gagnant != AUCUN) {
+          break;
+        }
+      }
+      return gagnant;
+    }
+
+    private int ieme(int[][] plateau, int i) {
+			return plateau[i*dx][i*dy];
 		}
-		System.out.println("+");
-	}
-	
+  }
+
+  private static class Position {
+    private int x, y;
+
+    private Position(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int getX() {
+      return x;
+    }
+
+    public int getY() {
+      return y;
+    }
+
+    public static Position lire(int[][] plateau, String prompt, Scanner clavier) {
+      int x = -1, y = -1;
+
+      do {
+        System.out.printf("%s, quelle case (ex: A1) ? ", prompt);
+        String position = clavier.next();
+
+        try {
+          x = position.charAt(0) - 'A';
+          y = position.charAt(1) - '1';
+        } catch (Exception e) {
+          System.err.println("Saisie incorrecte");
+        }
+      } while (x < 0 || TAILLE <= x || y < 0 || TAILLE <= y || plateau[x][y] != AUCUN);
+      return new Position(x, y);
+    }
+  }
+
+  private static int adversaire(int joueur) {
+    return JOUEUR1 + JOUEUR2 - joueur;
+  }
+
+  private static void dessinerLigne(int taille) {
+    for (int j = 0; j < taille; j++) {
+      System.out.print("+---");
+    }
+    System.out.println("+");
+  }
+
+  private static void dessinerPlateau(int[][] plateau) {
+    for (int x = 0; x < TAILLE; x++) {
+      System.out.printf("  %c ", 'A' + x);
+    }
+    System.out.println();
+    for (int y = 0; y < TAILLE; y++) {
+      dessinerLigne(TAILLE);
+      for (int x = 0; x < TAILLE; x++) {
+        System.out.printf("| %c ", PIONS.charAt(plateau[x][y]));
+      }
+      System.out.printf("| %d\n", y + 1);
+    }
+    dessinerLigne(TAILLE);
+
+  }
+
 }
