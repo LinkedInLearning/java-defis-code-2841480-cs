81d481bcf463db39d63e1e8abceb6aec52df2300
Labasse <labasse@gmail.com>
2022-09-07 09:05:44 +0000

    Savez-vous compter ? (solution)

--- src/ComptageMots.java
+++ src/ComptageMots.java
@@ -1,7 +1,36 @@
 public class ComptageMots {
+  private enum Etat {
+    Espace,
+    Mot
+  }
 
-	public static int compter(String phrase) {
-		throw new UnsupportedOperationException("Essayez votre propre solution ici !!!");
-	}
+  private final static String SEPARATEURS = "'-.;:?! \t\n\r";
 
+  private static boolean estUnSeparateur(char caractere) {
+    return SEPARATEURS.indexOf(caractere) >= 0;
+  }
+
+  public static int compter(String phrase) {
+    Etat etat = Etat.Espace;
+    int nombreMots = 0;
+
+    for (int i = 0; i < phrase.length(); i++) {
+      char caractere = phrase.charAt(i);
+
+      switch (etat) {
+        case Espace:
+          if (!estUnSeparateur(caractere)) {
+            etat = Etat.Mot;
+            nombreMots++;
+          }
+          break;
+        case Mot:
+          if (estUnSeparateur(caractere)) {
+            etat = Etat.Espace;
+          }
+          break;
+      }
+    }
+    return nombreMots;
+  }
 }
