b7c2d9a628a45963ad0c89e3c14bfea568479b62
Labasse <labasse@gmail.com>
2022-09-07 21:46:18 +0000

    Désamorcer la bombe (solution)

--- src/TriParPivot.java
+++ src/TriParPivot.java
@@ -1,21 +1,43 @@
+import java.util.Stack;
+
 public class TriParPivot {
-  public <T extends Comparable<? super T>> void trier(T[] tableau) {
-    trier(tableau, 0, tableau.length);
+  private class Parametres {
+    private final int premier, dernier;
+
+    public Parametres(int premier, int dernier) {
+      this.premier = premier;
+      this.dernier = dernier;
+    }
+
+    public int getPremier() {
+      return premier;
+    }
+
+    public int getDernier() {
+      return dernier;
+    }
   }
 
-  private <T extends Comparable<? super T>> void trier(T[] tableau, int premier, int dernier) {
-    if (premier >= dernier) {
-      return;
-    }
-    int pos = premier;
+  private Stack<Parametres> pile = new Stack<>();
 
-    for (int fin = dernier - 1; pos < fin;) {
-      permuter(
-          tableau, pos + 1,
-          tableau[pos + 1].compareTo(tableau[pos]) < 0 ? pos++ : fin--);
+  public <T extends Comparable<? super T>> void trier(T[] tableau) {
+    pile.push(new Parametres(0, tableau.length));
+
+    while (!pile.empty()) {
+      Parametres p = pile.pop();
+
+      if (p.getPremier() < p.getDernier()) {
+        int pos = p.getPremier();
+
+        for (int fin = p.getDernier() - 1; pos < fin;) {
+          permuter(
+              tableau, pos + 1,
+              tableau[pos + 1].compareTo(tableau[pos]) < 0 ? pos++ : fin--);
+        }
+        pile.push(new Parametres(p.getPremier(), pos));
+        pile.push(new Parametres(pos + 1, p.getDernier()));
+      }
     }
-    trier(tableau, premier, pos);
-    trier(tableau, pos + 1, dernier);
   }
 
   private static <T> void permuter(T[] tableau, int indice1, int indice2) {
